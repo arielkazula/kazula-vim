0ceeae5cfb2f8a1920c6d1a1a284f8a5e29ff403
Ariel Kazula <ariel.kazula@regatta.dev>
2026-03-31 15:22:04 +0300

    Feat: Migrate completion to blink.cmp and enhance navigation
    - Replaced nvim-cmp with blink.cmp (Rust-powered completion)
    - Integrated flash.nvim for advanced on-screen jumping
    - Modularized navigation plugins (FZF-Lua, Oil, Flash)
    - Configured lazydev.nvim for perfect Lua LSP support

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,50 @@
+-- lua/plugins/completion.lua ---------------------------------------------
+-- This file handles the autocompletion engine and snippet support.
+-- Using blink.cmp, a high-performance completion engine written in Rust.
+
+return {
+  {
+    "saghen/blink.cmp",
+    version = '*', -- Use latest release tag for pre-built binaries
+    dependencies = { 
+      "rafamadriz/friendly-snippets",
+      -- Add lazydev for better Neovim Lua API completion
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        ghost_text = { enabled = true },
+      },
+      sources = {
+        -- Added 'lazydev' to the default list of providers
+        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100, -- show at the top
+          },
+        },
+      },
+      signature = { enabled = true },
+    },
+    opts_extend = { "sources.default" },
+  },
+
+  -- Collection of common snippets for all languages
+  { "rafamadriz/friendly-snippets", lazy = true },
+}
